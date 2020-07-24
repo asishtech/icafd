d07807ebf8225865c47b4d057ca2a4bec67d20e8
asishtech <68698410+asishtech@users.noreply.github.com>
2020-07-24 17:16:43 +0530

    Add files via upload

---
+++ icafd.js
@@ -0,0 +1,38 @@
+   
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }
+
+    //Get the button:
+    mybutton = document.getElementById("myBtn");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () { scrollFunction() };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    } 
+
+    // function adj()
+    // {
+    //     var x = document.getElementById("myTopnav");
+    //     var y = document.getElementsByClassName("section");
+    //     for(var i = 0; i < y.length; i++){
+	// 	y[i].style.paddingTop = x.style.height;
+    // }
+}
